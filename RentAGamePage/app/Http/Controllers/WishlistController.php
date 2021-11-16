2821a2192bd44aff4a3c2f347a001db22bbec9a0
dpelaezv2 <dpelaezv2@eafit.edu.co>
2021-11-15 20:51:33 -0500

    PAGINA TERMINADA

--- RentAGamePage/app/Http/Controllers/WishlistController.php
+++ RentAGamePage/app/Http/Controllers/WishlistController.php
@@ -8,15 +8,25 @@ use Illuminate\Support\Facades;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use App\Models\VideoGame;
+use App\interfaces\RandomMovie;
 
 class WishlistController extends Controller
 {
 
     public function add(Request $request)
     {
+        $randomMovie = app(RandomMovie::class);
+        $movie = $randomMovie->random();
+        $data["movie"] = $movie;
+
+        $response = Http::get('https://catfact.ninja/fact');
+        $fact = json_decode($response->body());
+        $data["frase"] = $fact->fact;
+
+
         Wishlist::validate($request);
         Wishlist::create($request->only(["videoGameId","userId"]));
-        $data = VideoGame::all()->sortByDesc('admin');
+        $data["videogames"] = VideoGame::all()->sortByDesc('admin');
         return view('user.index')->with("data", $data);
     }
 
